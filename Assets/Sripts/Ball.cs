48b4e0f62fa2ae3e6c0ed9614ccc8b285b554a26
ixianren <649291929@qq.com>
2020-10-10 00:34:15 +0800

    second commit

--- Assets/Sripts/Ball.cs
+++ Assets/Sripts/Ball.cs
@@ -7,17 +7,16 @@ public class Ball : MonoBehaviour
     float _speed = 20f;
     Rigidbody _rigidbody;
     Vector3 _velocity;
-    // Start is called before the first frame update
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.velocity = Vector3.down * _speed;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
+        _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;//让小球的速度恒为20
         _velocity = _rigidbody.velocity;
     }
     private void OnCollisionEnter(Collision collision)
--- Assets/Sripts/Player.cs
+++ Assets/Sripts/Player.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody _rigidbody;
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.visible = false;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //_rigidbody.MovePosition(new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 50)).x, -17, 0));
+      _rigidbody.MovePosition(new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 50)).x, -17, 0));//相机的z为50,paddle的y每帧都设为-17
     }
 }
