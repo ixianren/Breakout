a821d56a2792be1d015302c72d57ccddf6cd2acf
ixianren <649291929@qq.com>
2020-10-10 23:12:28 +0800

    fifth commit

--- Assets/Sripts/Ball.cs
+++ Assets/Sripts/Ball.cs
@@ -7,17 +7,26 @@ public class Ball : MonoBehaviour
     float _speed = 20f;
     Rigidbody _rigidbody;
     Vector3 _velocity;
-
+    Renderer _renderer;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.velocity = Vector3.down * _speed;
+        Invoke("Launch", 0.5f);
+        _renderer = GetComponent<Renderer>();
     }
-
+    void Launch()
+    {
+        _rigidbody.velocity = Vector3.up * _speed;
+    }
     void FixedUpdate()
     {
         _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;//让小球的速度恒为20
         _velocity = _rigidbody.velocity;
+        if (!_renderer.isVisible)
+        {
+            GameManager.Instance.Balls--;
+            Destroy(gameObject);
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
--- Assets/Sripts/Player.cs
+++ Assets/Sripts/Player.cs
@@ -8,12 +8,11 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        Cursor.visible = false;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-      _rigidbody.MovePosition(new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 50)).x, -17, 0));//相机的z为50,paddle的y每帧都设为-17
+      _rigidbody.MovePosition(new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 50)).x, -25, 0));//相机的z为50,paddle的y每帧都设为-17
     }
 }
